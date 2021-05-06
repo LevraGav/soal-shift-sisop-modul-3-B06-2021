7ccd7b564f51f00e1a029eaae7c1810148759d83
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-05-07 00:16:53 +0700

    Update soal3server.c

--- soal3/soal3server.c
+++ soal3/soal3server.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -7,49 +9,51 @@
 #include <unistd.h>
 #define PORT 8080
 
-char sent[1024] = {};
-char recieve[1024] = {};
-char buff[1024] = {};
+char sent[1024];
+char recieve[1024];
+char buff[1024];
 char user[30];
 bool loggedIn = false;
 
 void addUser(char str[]) {
+    printf("ADDUSER\n");
     char idpass [100];
     char *token = strtok(str, " ");
     char id[30];
     strcpy(id,token);
     char password[30];
-    while (token!=null) {
+    while (token!=NULL) {
         token = strtok(NULL, " ");
     }
     strcpy(password,token);
     sprintf(idpass,"%s:%s",id,password);
     FILE* file = fopen("akun.txt", "a") ;
-    fputs(up,file);
+    fputs(idpass,file);
     fclose(file);
 }
 
-int addUser(char str[]) {
+bool LogUser(char str[]) {
     char idpass [100];
     char *token = strtok(str, " ");
     char id[30];
     strcpy(id,token);
     char password[30];
-    while (token!=null) {
+    while (token!=NULL) {
         token = strtok(NULL, " ");
     }
     strcpy(password,token);
     sprintf(idpass,"%s:%s",id,password);
     char find[100];
+    FILE* file = fopen("akun.txt","a");
     while (fgets(find,100,file)) {
         if (strcmp(find,idpass)==0) {
             fclose(file);
             strcpy(user,id);
             loggedIn = true;
-            return 1;
+            return true;
         }
     }
-    return 0;
+    return false;
 }
 
 int connectionCheck(){
@@ -61,14 +65,14 @@ int main(int argc, char const *argv[]) {
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
+    char buffer[1024];
       
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
       
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -93,26 +97,31 @@ int main(int argc, char const *argv[]) {
                 perror("accept");
                 exit(EXIT_FAILURE);
             }
+            printf("CONNECTED\n");
             connected = true;
             strcpy(sent,"Register / Login?\n");
+            printf("%s\n",sent);
             send(new_socket,sent,strlen(sent),0);
         }
         else {
-            read(new_socket,recieve,0)
-            if(connectionCheck) {
+            read(new_socket,recieve,1024);
+            printf("Recieve: %s\n",recieve);
+            if(connectionCheck()) {
                 if (strcmp(recieve,"register")==0) {
+                    printf("register\n");
                     strcpy(sent,"Register\nInput ID dan password dipisah oleh spasi\n Ex: Bayu 123");
                     send(new_socket,sent,strlen(sent),0);
-                    read(new_socket,recieve,0);
+                    read(new_socket,recieve,1024);
                     addUser(recieve);
                     strcpy(sent,"Registrasi berhasil, silahkan login!\n");
                     send(new_socket,sent,strlen(sent),0);
                     continue;
                 }
                 if (strcmp(recieve,"login")==0) {
+                    printf("login\n");
                     strcpy(sent,"Login\nInput ID dan password dipisah oleh spasi\n Ex: Bayu 123");
                     send(new_socket,sent,strlen(sent),0);
-                    read(new_socket,recieve,0);
+                    read(new_socket,recieve,1024);
                     if (LogUser(recieve)) {
                         strcpy(sent,"Login sukses\n");
                         send(new_socket,sent,strlen(sent),0);
@@ -126,8 +135,7 @@ int main(int argc, char const *argv[]) {
             else {
                 connected = 0;
             }
-        }
-        
+        }   
     }
     return 0;
 }
