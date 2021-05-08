2991d27fea9068f6364358395136f729c708cfd3
Faisal Reza <7farema@gmail.com>
2021-05-08 21:22:01 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,1 +1,17 @@
+#include<sys/types.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<ctype.h>
+#include<dirent.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<string.h>
 
+int main(int argc, char *argv[])
+{
+    if(strcmp(argv[1], "-f") == 0)
+    {
+        printf("parameter -f dipilih\n");
+    }
+}
